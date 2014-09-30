0c12d78e941468ceb3e4573a0e1afe8778b6764a
remluben <ulmer.benjamin@gmail.com>
2014-09-30 22:19:05 +0200

    Added phpmyadmin to boilerplates, database name and MYSQL root password may be set from variables now within the install.sh scripts

--- boilerplates/lamp-laravel/install.sh
+++ boilerplates/lamp-laravel/install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+# adjust to whatever you like as a MYSQL root password
+# adjust to whatever you like as a MYSQL database name
+DBPASSWD=root
+DBNAME=development
+
 echo "--- Good morning, master. Let's get to work. Installing now. ---"
 
 echo "--- Updating packages list ---"
 sudo apt-get update
-
-echo "--- MySQL time ---"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
 echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties
@@ -17,6 +18,18 @@ sudo add-apt-repository -y ppa:ondrej/php5
 
 echo "--- Updating packages list ---"
 sudo apt-get update
+
+echo "--- MySQL time ---"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+
+echo "\n--- Installing the phpmyadmin for easier MYSQL database access. It will save you time master. ---\n"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
+sudo apt-get -y install mysql-server-5.5 phpmyadmin
 
 echo "--- Installing PHP-specific packages ---"
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
@@ -46,6 +59,20 @@ sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 echo "--- Allowing .htaccess overrides. You will need it master. ---"
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 
+echo "\n--- Configuring Apache webserver to use phpmyadmin ---\n"
+echo "Listen 81" >> /etc/apache2/ports.conf
+
+cat > /etc/apache2/conf-available/phpmyadmin.conf << "EOF"
+<VirtualHost *:81>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /usr/share/phpmyadmin
+    DirectoryIndex index.php
+    ErrorLog ${APACHE_LOG_DIR}/phpmyadmin-error.log
+    CustomLog ${APACHE_LOG_DIR}/phpmyadmin-access.log combined
+</VirtualHost>
+EOF
+sudo a2enconf phpmyadmin
+
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
 
@@ -53,18 +80,19 @@ echo "--- Composer is the future. But you knew that, did you master? Nice job. -
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "--- Setup database development ---"
-echo "CREATE DATABASE development;" | mysql -u root -proot
+echo "--- Setup database $DBNAME ---"
+echo "CREATE DATABASE $DBNAME;" | mysql -u root -p$DBPASSWD
 
 echo "--- We need laravel installed for out laravel project. Don't we? ---"
 composer create-project laravel/laravel /vagrant/laravel --prefer-dist
 mv /vagrant/laravel/* /vagrant
 rm -r /vagrant/laravel
+
 echo "--- Activate laravel debug mode. ---"
 sed -i "s/'debug' => false,*/'debug' => true,/" /vagrant/app/config/app.php
 echo "--- Set laravel database credentials. ---"
-sed -i "s/'database'  => .*/'database'  => 'development',/" /vagrant/app/config/database.php
+sed -i "s/'database'  => .*/'database'  => '$DBNAME',/" /vagrant/app/config/database.php
 sed -i "s/'username'  => .*/'username'  => 'root',/" /vagrant/app/config/database.php
-sed -i "s/'password'  => .*/'password'  => 'root',/" /vagrant/app/config/database.php
+sed -i "s/'password'  => .*/'password'  => '$DBPASSWD',/" /vagrant/app/config/database.php
 
 echo "--- All set to go! Would you like to play a game? ---"
--- boilerplates/lamp/install.sh
+++ boilerplates/lamp/install.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+# adjust to whatever you like as a MYSQL root password
+# adjust to whatever you like as a MYSQL database name
+DBPASSWD=root
+DBNAME=development
+
 echo "--- Good morning, master. Let's get to work. Installing now. ---"
 
 echo "--- Updating packages list ---"
 sudo apt-get update
 
 echo "--- MySQL time ---"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
 
 echo "--- Installing base packages ---"
 sudo apt-get install -y vim curl python-software-properties
@@ -17,6 +22,18 @@ sudo add-apt-repository -y ppa:ondrej/php5
 
 echo "--- Updating packages list ---"
 sudo apt-get update
+
+echo "\n--- Installing MySQL specific packages and settings ---\n"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+
+echo "\n--- Installing the phpmyadmin for easier MYSQL database access. It will save you time master. ---\n"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
+sudo apt-get -y install mysql-server-5.5 phpmyadmin
 
 echo "--- Installing PHP-specific packages ---"
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core
@@ -46,6 +63,20 @@ sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 echo "--- Allowing .htaccess overrides. You will need it master. ---"
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 
+echo "\n--- Configuring Apache webserver to use phpmyadmin ---\n"
+echo "Listen 81" >> /etc/apache2/ports.conf
+
+cat > /etc/apache2/conf-available/phpmyadmin.conf << "EOF"
+<VirtualHost *:81>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /usr/share/phpmyadmin
+    DirectoryIndex index.php
+    ErrorLog ${APACHE_LOG_DIR}/phpmyadmin-error.log
+    CustomLog ${APACHE_LOG_DIR}/phpmyadmin-access.log combined
+</VirtualHost>
+EOF
+sudo a2enconf phpmyadmin
+
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
 
@@ -53,7 +84,7 @@ echo "--- Composer is the future. But you knew that, did you master? Nice job. -
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "--- Setup database development ---"
-echo "CREATE DATABASE development;" | mysql -u root -proot
+echo "--- Setup database $DBNAME ---"
+echo "CREATE DATABASE $DBNAME;" | mysql -u root -p$DBPASSWD
 
 echo "--- All set to go! Would you like to play a game? ---"
